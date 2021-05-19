836042c1ac491dca1248c00fc8ff1691030766e9
Kano <takayukikano.0613@gmail.com>
2021-05-19 15:21:49 +0900

    Ninja code:Vue.js

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -16,28 +16,15 @@
 </template>
 
 <style>
-#app {
-    font-size: 3rem;
-    font-weight: bold;
-    text-align: center;
-    margin: 30% auto;
-    color: #41BA83;
-}
+
 </style>
 
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
-
-    const vm = new Vue({
+  const vm = new Vue({
     el: "#app",
     data: {
       message: "Hello Vue.js!!",
     },
-    });
-
+  });
 </script>
